b5885568a76b52d384740081b7d901682d185dad
Ahmed Kouki <75638871+koukiahmed@users.noreply.github.com>
2022-05-10 01:31:32 +0200

    Add files via upload

---
+++ js/file.js
@@ -0,0 +1,72 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
+
+
+//function for score
+let scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+
+//start Game
+window.addEventListener("keydown", (start) => {
+    //console.log(start);
+    if (start.code == "Space") {
+        gameOver.style.display = "none";
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+
+        //score
+        let playerScore = 0;
+        interval = setInterval(scoreCounter, 500);
+    }
+});
+
+
+//jump Your dino
+window.addEventListener("keydown", (e) => {
+    //console.log(e);
+
+    if (e.key == "ArrowUp")
+        if (dino.classList != "dinoActive") {
+            dino.classList.add("dinoActive");
+
+            //remove class after 0.5 seconds
+            setTimeout(() => {
+                dino.classList.remove("dinoActive");
+            }, 500);
+        }
+});
+
+//Game Over if dino hit The 'Block' 
+let result = setInterval(() => {
+    let dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue("bottom"));
+    //convert position from px to number
+    //console.log("dinoBottom" + dinoBottom);
+
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
+    //convert position from px to number
+    //console.log("BlockLeft" + blockLeft);
+
+    if (dinoBottom <= 90 && blockLeft >= 20 && blockLeft <= 50) {
+        //console.log("Game Over");
+
+        gameOver.style.display = "block";
+        block.classList.remove("blockActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
+    }
+}, 10);
